4412824116b32c706330d72a108e26337dfe5bf1
Victor <VictorCarmona9035@hotmail.com>
2022-08-13 21:10:00 +0200

    1-Agregando elementos de forma dinámica y Eliminar elementos creados de forma dinámica

--- src/app/template/dinamicos/dinamicos.component.ts
+++ src/app/template/dinamicos/dinamicos.component.ts
@@ -1,14 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
-// interface Persona{
-//   nombre:string;
-//   favoritos:Favorito[];
-// }
+interface Persona{
+  nombre:string;
+  favoritos:Favorito[];
+}
 
-// interface Favorito{
-//   id:number;
-//   nombre:string;
-// }
+interface Favorito{
+  id:number;
+  nombre:string;
+}
 
 
 @Component({
@@ -16,15 +16,24 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './dinamicos.component.html',
   styleUrls: ['./dinamicos.component.css']
 })
-export class DinamicosComponent implements OnInit {
+export class DinamicosComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+persona:Persona={
+  nombre:'Fernadno',
+  favoritos:[
+    {id:1, nombre:'Metal Gear'},
+    {id:2, nombre:'Fifa 2022'},
+  ]
+}
 
   guardar(){
     console.log('formulario posteado');
+  }
+
+  //Elimina un elemento del listado de favoritos por su id
+  eliminar(index:number){
+    //Borramos del arreglo
+    this.persona.favoritos.splice(index,1);
   }
 
 }
