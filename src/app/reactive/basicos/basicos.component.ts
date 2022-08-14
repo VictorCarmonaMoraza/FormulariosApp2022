dc6eb80f211c614f2a8669c2ced2fe0cc6de204b
Victor <VictorCarmona9035@hotmail.com>
2022-08-14 20:42:48 +0200

    19-Submit del formulario

--- src/app/reactive/basicos/basicos.component.ts
+++ src/app/reactive/basicos/basicos.component.ts
@@ -1,19 +1,14 @@
 import { Validators } from '@angular/forms';
 import { FormBuilder, FormGroup } from '@angular/forms';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-basicos',
   templateUrl: './basicos.component.html',
   styleUrls: ['./basicos.component.css']
 })
-export class BasicosComponent {
+export class BasicosComponent implements OnInit {
 
-  // miFormulario:FormGroup=new FormGroup({
-  //   'nombre':new FormControl('RTX 4080ti'),
-  //   'precio':new FormControl(1500),
-  //   'existencias':new FormControl(0)
-  // })
   miFormulario: FormGroup = this.fb.group({
     nombre: [, [Validators.required, Validators.minLength(3)]],
     precio: [, [Validators.min(0), Validators.required]],
@@ -22,14 +17,35 @@ export class BasicosComponent {
 
   constructor(private fb: FormBuilder) { }
 
+  ngOnInit(): void {
+    this.miFormulario.reset({
+      nombre:'RTX 4080ti',
+      precio:1600,
+
+    })
+  }
+
   // campoEsValido() {
   //   return this.miFormulario.controls['nombre'].errors
   //     && this.miFormulario.controls['nombre'].touched
   // }
 
-  campoEsValido(campo:string){
+  campoEsValido(campo: string) {
     return this.miFormulario.controls[campo].errors
-       && this.miFormulario.controls[campo].touched
+      && this.miFormulario.controls[campo].touched
+  }
+
+  guardar() {
+    if (this.miFormulario.invalid) {
+      //Comprueba si alguno o todos los campos han sido tocados
+      this.miFormulario.markAllAsTouched();
+        return;
+    }
+
+    //Imprimimos los valores del formulario
+    console.log(this.miFormulario.value);
+    //Resetea el formulario
+    this.miFormulario.reset();
   }
 
 }
