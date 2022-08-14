b5239895964d2ac5fcee00c8bfacefc896afedca
Victor <VictorCarmona9035@hotmail.com>
2022-08-14 19:55:49 +0200

    18-Mostrar mensajes de error

--- src/app/reactive/basicos/basicos.component.ts
+++ src/app/reactive/basicos/basicos.component.ts
@@ -1,27 +1,35 @@
 import { Validators } from '@angular/forms';
-import { FormBuilder,FormGroup } from '@angular/forms';
-import { Component} from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-basicos',
   templateUrl: './basicos.component.html',
   styleUrls: ['./basicos.component.css']
 })
-export class BasicosComponent{
+export class BasicosComponent {
 
   // miFormulario:FormGroup=new FormGroup({
   //   'nombre':new FormControl('RTX 4080ti'),
   //   'precio':new FormControl(1500),
   //   'existencias':new FormControl(0)
   // })
-miFormulario:FormGroup=this.fb.group({
-  nombre:['RTX 40480ti',[Validators.required,Validators.minLength(3)]],
-  precio:[0,[Validators.min(0),Validators.required]],
-  existencias:[0,[Validators.min(0),Validators.required]],
-})
+  miFormulario: FormGroup = this.fb.group({
+    nombre: [, [Validators.required, Validators.minLength(3)]],
+    precio: [, [Validators.min(0), Validators.required]],
+    existencias: [, [Validators.min(0), Validators.required]],
+  })
 
-  constructor(private fb:FormBuilder) { }
+  constructor(private fb: FormBuilder) { }
 
+  // campoEsValido() {
+  //   return this.miFormulario.controls['nombre'].errors
+  //     && this.miFormulario.controls['nombre'].touched
+  // }
 
+  campoEsValido(campo:string){
+    return this.miFormulario.controls[campo].errors
+       && this.miFormulario.controls[campo].touched
+  }
 
 }
